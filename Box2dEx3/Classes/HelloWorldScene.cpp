2043c2e2d89d06a1a62125c2097cd1804837099b
Hyegon.Kim <cow9311@gmail.com>
2019-04-19 20:45:54 +0900

    Box2dEx Body types
    Box2dEx3 : Static Body, Kinematic Body, Dynamic Body

---
+++ Box2dEx3/Classes/HelloWorldScene.cpp
@@ -0,0 +1,297 @@
+﻿/****************************************************************************
+ Copyright (c) 2017-2018 Xiamen Yaji Software Co., Ltd.
+ 
+ http://www.cocos2d-x.org
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ ****************************************************************************/
+
+#include "HelloWorldScene.h"
+#include "SimpleAudioEngine.h"
+
+USING_NS_CC;
+
+Scene *HelloWorld::createScene()
+{
+	return HelloWorld::create();
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+	//////////////////////////////
+	// 1. super init first
+	if (!Scene::init())
+	{
+		return false;
+	}
+
+	winSize = Director::getInstance()->getWinSize();
+	texture = Director::getInstance()->getTextureCache()->addImage("blocks.png");
+
+	createBox2dWorld();
+	setBox2dWorld();
+	schedule(schedule_selector(HelloWorld::tick));
+
+	return true;
+}
+
+void HelloWorld::createBox2dWorld()
+{
+	b2Vec2 gravity = b2Vec2(0.0f, -30.0f);
+
+	_world = new b2World(gravity);
+	_world->SetAllowSleeping(true);
+	_world->SetContinuousPhysics(true);
+
+#ifdef BOX2D_DEBUG
+	// 디버그 드로잉 설정
+	// 적색 : 현재 물리 운동을 하는 것
+		// 회색 : 현재 물리 운동량이 없는 것
+	m_debugDraw = new GLESDebugDraw(PTM_RATIO);
+	_world->SetDebugDraw(m_debugDraw);
+
+	uint32 flags = 0;
+	flags += b2Draw::e_shapeBit;
+	//flags += b2Draw::e_jointBit;
+	//flags += b2Draw::e_aabbBit;
+	//flags += b2Draw::e_pairBit;
+	//flags += b2Draw::e_centerOfMassBit;
+	m_debugDraw->SetFlags(flags);
+#else
+	auto* wlayer = LayerColor::create(Color4B(255, 255, 255, 255));
+	this->addChild(wlayer);
+#endif // BOX2D_DEBUG
+
+	b2BodyDef groundBodyDef;
+	groundBodyDef.position.Set(0, 0);
+	b2Body* groundBody = _world->CreateBody(&groundBodyDef);
+
+	b2EdgeShape groundEdge;
+	b2FixtureDef boxShapeDef;
+	boxShapeDef.shape = &groundEdge;
+
+	groundEdge.Set(b2Vec2(0, 0), b2Vec2(winSize.width / PTM_RATIO, 0));
+	groundBody->CreateFixture(&boxShapeDef);
+
+	groundEdge.Set(b2Vec2(0, 0), b2Vec2(0, winSize.height / PTM_RATIO));
+	groundBody->CreateFixture(&boxShapeDef);
+
+	groundEdge.Set(b2Vec2(0, winSize.height / PTM_RATIO), b2Vec2(winSize.width / PTM_RATIO, winSize.height / PTM_RATIO));
+	groundBody->CreateFixture(&boxShapeDef);
+
+	groundEdge.Set(b2Vec2(winSize.width / PTM_RATIO, winSize.height / PTM_RATIO), b2Vec2(winSize.width / PTM_RATIO, 0));
+	groundBody->CreateFixture(&boxShapeDef);
+}
+
+void HelloWorld::setBox2dWorld()
+{
+	Sprite* pSprite1 = Sprite::createWithTexture(texture, Rect(0, 0, 64, 64));
+	pSprite1->setPosition(Vec2(winSize.width / 2, winSize.height / 2));
+	//this->addChild(pSprite1);
+
+	b2BodyDef bodyDef1;
+	bodyDef1.type = b2_staticBody;
+	bodyDef1.position.Set(winSize.width / 2 / PTM_RATIO, winSize.height / 2 / PTM_RATIO);
+	bodyDef1.userData = nullptr;
+
+	b2Body * body1 = _world->CreateBody(&bodyDef1);
+	b2PolygonShape staticBox;
+	staticBox.SetAsBox((pSprite1->getContentSize().width / 2) / PTM_RATIO, (pSprite1->getContentSize().height / 2) / PTM_RATIO);
+	
+	b2FixtureDef fixtureDef1;
+	fixtureDef1.shape = &staticBox;
+	fixtureDef1.density = 1.0f;
+	body1->CreateFixture(&fixtureDef1);
+
+
+	Sprite * pSprite2 = Sprite::createWithTexture(texture, Rect(0, 0, 64, 32));
+	pSprite2->setPosition(Vec2(0, 100));
+	//this->addChild(pSprite2);
+
+	b2BodyDef bodyDef2;
+	bodyDef2.type = b2_kinematicBody;
+	bodyDef2.position.Set(0, 150.0f / PTM_RATIO);
+	bodyDef2.linearVelocity = b2Vec2(3.0f, 0);
+	bodyDef2.userData = nullptr;
+
+	b2Body * body2 = _world->CreateBody(&bodyDef2);
+	b2PolygonShape kinematicBox;
+	kinematicBox.SetAsBox((pSprite2->getContentSize().width / 2) / PTM_RATIO, (pSprite2->getContentSize().height / 2) / PTM_RATIO);
+
+	b2FixtureDef fixtureDef2;
+	fixtureDef2.shape = &kinematicBox;
+	fixtureDef2.density = 1.0f;
+	body2->CreateFixture(&fixtureDef2);
+}
+
+HelloWorld::~HelloWorld()
+{
+	delete _world;
+	_world = nullptr;
+}
+
+#ifdef BOX2D_DEBUG
+void HelloWorld::draw(Renderer * renderer, const Mat4 & transform, uint32_t flags)
+{
+	Scene::draw(renderer, transform, flags);
+
+	_customCmd.init(_globalZOrder, transform, flags);
+	_customCmd.func = CC_CALLBACK_0(HelloWorld::onDraw, this, transform, flags);
+	renderer->addCommand(&_customCmd);
+}
+
+void HelloWorld::onDraw(const Mat4 & transform, uint32_t flags)
+{
+	Director* director = Director::getInstance();
+	CCASSERT(nullptr != director, "Director is null when setting matrix stack");
+	director->pushMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW);
+	director->loadMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW, transform);
+
+	GL::enableVertexAttribs(cocos2d::GL::VERTEX_ATTRIB_FLAG_POSITION);
+	_world->DrawDebugData();
+	CHECK_GL_ERROR_DEBUG();
+
+	director->popMatrix(MATRIX_STACK_TYPE::MATRIX_STACK_MODELVIEW);
+}
+#endif // BOX2D_DEBUG
+
+void HelloWorld::onEnter()
+{
+	Scene::onEnter();
+
+	auto* listener = EventListenerTouchOneByOne::create();
+	listener->setSwallowTouches(true);
+	listener->onTouchBegan = CC_CALLBACK_2(HelloWorld::onTouchBegan, this);
+
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+}
+
+void HelloWorld::onExit()
+{
+	Scene::onExit();
+}
+
+void HelloWorld::tick(float deltaTime)
+{
+	int32_t velocityIterations = 8;
+	int32_t positionIterations = 3;
+
+	_world->Step(deltaTime, velocityIterations, positionIterations);
+
+	for (b2Body* body = _world->GetBodyList(); body != nullptr; body = body->GetNext())
+	{
+		if (body->GetUserData() != nullptr)
+		{
+			Sprite* spriteData = (Sprite*)body->GetUserData();
+			spriteData->setPosition(Vec2(body->GetPosition().x * PTM_RATIO, body->GetPosition().y * PTM_RATIO));
+			spriteData->setRotation(-1 * CC_RADIANS_TO_DEGREES(body->GetAngle()));
+		}
+		if (body->GetType() == b2_kinematicBody)
+		{
+			b2Vec2 v = body->GetPosition();
+			if (v.x * PTM_RATIO > winSize.width) 
+			{
+				body->SetLinearVelocity(b2Vec2(-3.0f, 0));
+			}
+			else if (v.x < 0) 
+			{
+				body->SetLinearVelocity(b2Vec2(3.0f, 0));
+			}
+		}
+	}
+}
+
+bool HelloWorld::onTouchBegan(Touch* touch, Event* event)
+{
+	Vec2 touchPoint = touch->getLocation();
+	addNewSpriteAtPosition(touchPoint);
+
+	return true;
+}
+
+void HelloWorld::addNewSpriteAtPosition(Vec2 location)
+{
+	b2BodyDef bodyDef;
+	bodyDef.type = b2_dynamicBody;
+	bodyDef.position.Set(location.x / PTM_RATIO, location.y / PTM_RATIO);
+
+#ifdef BOX2D_DEBUG	
+	bodyDef.userData = nullptr;
+#else
+
+#endif // BOX2D_DEBUG
+	b2Body* body = _world->CreateBody(&bodyDef);
+
+	int nNum = rand() % 3;
+
+	if (nNum == 0) 
+	{
+		b2PolygonShape dynamicBox;
+		dynamicBox.SetAsBox(0.8f, 0.8f);
+
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &dynamicBox;
+		fixtureDef.density = 1.0f;
+		fixtureDef.friction = 0.3f;
+		fixtureDef.restitution = 0.0f;
+
+		body->CreateFixture(&fixtureDef);
+	}
+	else if (nNum == 1) 
+	{
+		b2PolygonShape dynamicBox;
+		b2Vec2 tri[4];
+
+		tri[0].x = -0.5;
+		tri[0].y = 0.0;
+
+		tri[1].x = 0.5;
+		tri[1].y = 0.0;
+
+		tri[2].x = 0;
+		tri[2].y = 1.0;
+
+		tri[3].x = 0.5;
+		tri[3].y = 1.0;
+
+		dynamicBox.Set(tri, 4);
+
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &dynamicBox;
+		fixtureDef.density = 1.0f;
+		fixtureDef.friction = 0.3f;
+		fixtureDef.restitution = 1.0f;
+
+		body->CreateFixture(&fixtureDef);
+	}
+	else 
+	{
+		b2CircleShape dynamicCircle;
+		dynamicCircle.m_radius = 1.0;
+
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &dynamicCircle;
+		fixtureDef.density = 1.0f;
+		fixtureDef.friction = 0.2f;
+		fixtureDef.restitution = 1.0f;
+
+		body->CreateFixture(&fixtureDef);
+	}
+}
